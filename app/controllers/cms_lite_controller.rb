a07756d060479bf0772ca285334b1d90c80495da
Justin Ball <justinball@gmail.com>
2009-05-06 19:13:03 -0600

    changes to how formats are handled

--- app/controllers/cms_lite_controller.rb
+++ app/controllers/cms_lite_controller.rb
@@ -12,7 +12,10 @@ class CmsLiteController < ApplicationController
   
   def render_content_page(cms_lite_path, request_layout = '')
     path = File.join(RAILS_ROOT, CmsLite::CMS_LITE_CONTENT_PATH, cms_lite_path, I18n.locale.to_s, params[:content_key], params[:content_page].join('/'))
-    content_page = Dir.glob("/#{path}.*").first
+    format = params[:format] || 'htm'
+    content_page = Dir.glob("/#{path}.#{format}").first
+    content_page = Dir.glob("/#{path}").first  if content_page.nil?
+    content_page = Dir.glob("/#{path}.*").first  if content_page.nil?
     raise CmsLiteExceptions::MissingTemplateError, "Could not find template for: '#{path}'" if content_page.nil?
     respond_to do |format|
       format.html { render :file => content_page }
@@ -20,6 +23,7 @@ class CmsLiteController < ApplicationController
       format.xml { render :file => content_page, :layout => false }
     end
   rescue CmsLiteExceptions::MissingTemplateError => ex
+    
     render :file => "#{RAILS_ROOT}/public/404.html", :status => 404
   end
   
